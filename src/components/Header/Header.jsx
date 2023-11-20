df61cedd8867f076e44a54416c0644d5c9dbc5f4
Junnam2806 <namtruong286@gmail.com>
2023-11-21 00:59:18 +0700

    This is the second commit on git

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,9 @@ const Header = () => {
     <div className="container">
         <div className='flex items-center justify-between'>
             <div className="flex items-center gap-[10px]">
-                <span className='w-[35px] h-[35px] bg-primaryColor text-white'></span>
+                <span className='w-[35px] h-[35px] bg-primaryColor text-white text-[18px] font-[500] rounded-full flex items-center justify-center'>
+                  M
+                </span>
             </div>
         </div>
     </div>
